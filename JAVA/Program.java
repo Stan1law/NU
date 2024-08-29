efbc54574e247e0b1858cb3ecd50605b0f48f9d9
Stan1law <tantanmorallo213@gmail.com>
2024-08-29 21:33:37 +0800

    Activities

---
+++ JAVA/Program.java
@@ -0,0 +1,22 @@
+package JAVA;
+
+public class Program {
+
+    public static double dollarToPesos(double dollars) {
+        double exchangeRate = 51.50;
+        return dollars * exchangeRate;
+    }
+
+    public static double inch_to_cm(double inches) {
+        return inches * 2.54;
+    }
+
+    public static int Fahren_to_Cel(int Fahrenheit) {
+        return (int) ((Fahrenheit - 32) * 5 / 9);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
